5688f6695c5e9c4bed021bcf9c1bae2b323d018f
Martin Bil <mb@martin-bil.cz>
2024-04-03 18:08:35 +0200

    db configuration

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Adds x and y and returns the value."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtracts x from y and returns the value."""
+    return y - x
